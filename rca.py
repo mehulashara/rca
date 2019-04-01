9ef29264d45b2718ee30f4a3231b5558358fe6eb
mehulashara <mehulashara017@gmail.com>
2019-04-01 15:18:06 +0530

    Update rca.py

--- rca.py
+++ rca.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python
+#------------------------------------------------------------------------#
+# File name: rca.py
+# Author: Mehul Ashara  
+# Email: mehul.ashara@nutanix.com
+# Date created: 04/01/2019
+# Date last modifed: 04/01/2019
+# Python version: 2.7.5
+# Purpose: This hypervisor agnostic script to be run from CVM can be used to 
+# simply rca process for SREs 
+# Usage: rca.py >> /home/nutanix/tmp/rca_"$(date +"%Y-%m-%d_%H-%M").log"
+#-------------------------------------------------------------------------#
 import subprocess
 import sys
 
